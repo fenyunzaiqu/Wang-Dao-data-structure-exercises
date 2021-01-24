3c0ca7efa977693ea8c533716bc30cffaaaeef59
fenyunzaiqu <41602695+fenyunzaiqu@users.noreply.github.com>
2021-01-25 04:40:04 +0800

    Add files via upload
    单链表的增删查改操作

---
+++ main.cpp
@@ -0,0 +1,78 @@
+//
+//  main.cpp
+//  danlianbiao
+//
+//  Created by 周南 on 2021/1/24.
+//
+
+#include <iostream>
+using namespace::std;
+typedef struct Node{
+    int data;
+    struct Node *next;//因为指针要和它指向的变量是一个数据类型，next指针要指向下一个Lnode，所以也要用Lnode来定义
+}LNode, *Linklist;//等同于Node定义了Linklist和LNode，这里下面的Lnode *强调返回的是一个结点（和上面的Node不是一个东西，上面的Node是方法），如Lnode * GetElem(Linklist L, int i)一目了然
+
+Linklist List_HeadInsert(Linklist &L);//通过头插法来初始化单链表
+Linklist List_TrailInsert(Linklist &L);//通过尾插法来初始化单链表
+LNode *GetElem(Linklist L,int i);//按序号查找节点值
+
+int main()
+{
+    Linklist L;
+    List_TrailInsert(L);
+    cout<<GetElem(L, 3)->data<<endl;
+    cout<<"hello"<<endl;
+    return 0;
+}
+
+//头插法
+Linklist List_HeadInsert(Linklist &L)
+{
+    Node *s;//定义节点s
+    L=(Linklist)malloc(sizeof(Node));
+    //定义头节点L,并申请内存空间，其中Linklist也可以换成Node*，是一个东西，代表申请空间的数据类型是啥。当没有这个头节点L的时候，需要在每次插入新节点后将它的地址赋值给L 即L=s;
+    L->next=NULL;
+    int A[]={1,3,4,5,6,12,3,14,5};
+    for(int i=0;i<(sizeof(A)/sizeof(int));i++)
+    {
+        s=(Node*)malloc(sizeof(Node));
+        s->data=A[i];
+        s->next=L->next;
+        L->next=s;
+    }
+    return L;
+}
+
+//尾插法
+Linklist List_TrailInsert(Linklist &L)
+{
+    Node *s,*r=L;//需要多一个尾指针r来指向最后一个将要插入的元素
+    L=(Linklist)malloc(sizeof(Node));//因为不需要关心L->next指向哪里，所以也不需要初始化空链表了
+    int A[]={1,2,13,21,25,3,2,7,8,1,2,3};
+    for(int i=0;i<sizeof(A)/sizeof(int);i++)
+    {
+        s=(Node*)malloc(sizeof(Node));
+        s->data=A[i];
+        r->next=s;
+        r=s;
+    }
+    r->next=NULL;//最后赋值初始化完需要将尾指针置空来达到“它是尾指针”的目的
+    return L;
+}
+
+//按序号查找节点值
+LNode *GetElem(Linklist L,int i)
+{
+    int j=1;//用来计数
+    LNode *p=L->next;
+    if(i==0)
+        return L;
+    if(i<1)
+        return NULL;
+    while(p&&j<i)//p不为空（null)即最后一个，那么就找到第i个位置
+    {
+        p=p->next;//不断指下去
+        j++;
+    }
+    return p;//返回p这个节点
+}
